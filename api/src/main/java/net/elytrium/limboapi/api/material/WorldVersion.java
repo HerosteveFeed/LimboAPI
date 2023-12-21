b4638318f3df1ae2717267ebd54c01f10bf1b687
AltronMaxX <max06112004@gmail.com>
2023-12-22 00:11:22 +0400

    try that

--- api/src/main/java/net/elytrium/limboapi/api/material/WorldVersion.java
+++ api/src/main/java/net/elytrium/limboapi/api/material/WorldVersion.java
@@ -27,7 +27,8 @@ public enum WorldVersion {
   MINECRAFT_1_19_4(EnumSet.range(ProtocolVersion.MINECRAFT_1_19_4, ProtocolVersion.MINECRAFT_1_19_4)),
   MINECRAFT_1_20(EnumSet.range(ProtocolVersion.MINECRAFT_1_20, ProtocolVersion.MINECRAFT_1_20)),
   MINECRAFT_1_20_2(EnumSet.range(ProtocolVersion.MINECRAFT_1_20_2, ProtocolVersion.MINECRAFT_1_20_2)),
-  MINECRAFT_1_20_3(EnumSet.range(ProtocolVersion.MINECRAFT_1_20_3, ProtocolVersion.MAXIMUM_VERSION));
+  MINECRAFT_1_20_3(EnumSet.range(ProtocolVersion.MINECRAFT_1_20_3, ProtocolVersion.MAXIMUM_VERSION)),
+  MINECRAFT_1_20_4(EnumSet.range(ProtocolVersion.MINECRAFT_1_20_3, ProtocolVersion.MAXIMUM_VERSION));
 
   private static final EnumMap<ProtocolVersion, WorldVersion> MC_VERSION_TO_ITEM_VERSIONS = new EnumMap<>(ProtocolVersion.class);
 
@@ -97,6 +98,9 @@ public enum WorldVersion {
       }
       case "1.20.3": {
         return MINECRAFT_1_20_3;
+      }
+      case "1.20.4": {
+        return MINECRAFT_1_20_4;
       }
       default: {
         return LEGACY;
